3bc1f203deea103b2a5d48af3bc8e6b3f5cb7a27
Aviv Beeri <aviv.beeri@forgerock.com>
2020-10-21 18:48:11 +0100

    adding test formula

---
+++ Formula/dome.rb
@@ -0,0 +1,20 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class dome < Formula
+  desc "The comfortable framework for making games in Wren."
+  homepage "https://domeengine.com"
+  url "https://github.com/avivbeeri/dome/releases/latest/download/"
+  version "1.3.0"
+  sha256 ""
+  license "MIT"
+
+  def install
+    system "make"
+    cp_r "dome" bin/"dome"
+  end
+
+  test do
+    system "true"
+  end
+end
