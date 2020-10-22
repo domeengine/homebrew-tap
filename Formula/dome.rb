a3dfeee939b4ac6d9177fc0dd3b02ea5149f9b5f
Aviv Beeri <aviv.beeri@forgerock.com>
2020-10-22 11:59:03 +0100

    fixing invalid syntax

--- Formula/dome.rb
+++ Formula/dome.rb
@@ -1,7 +1,7 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class DOME < Formula
+class Dome < Formula
   desc "The comfortable framework for making games in Wren."
   homepage "https://domeengine.com"
   url "https://github.com/avivbeeri/dome/releases/latest/download/"
