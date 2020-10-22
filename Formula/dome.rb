74a1942fefb23860045092ca7d0cbb61edd36cd3
Aviv Beeri <aviv.beeri@forgerock.com>
2020-10-22 12:30:51 +0100

    swapped to git

--- Formula/dome.rb
+++ Formula/dome.rb
@@ -4,18 +4,14 @@
 class Dome < Formula
   desc "The comfortable framework for making games in Wren."
   homepage "https://domeengine.com"
-  url "https://github.com/avivbeeri/dome/releases/download/v1.3.0/dome-1.3.0-macosx-x64.zip"
   version "1.3.0"
   sha256 ""
   license "MIT"
+  head "https://github.com/avivbeeri/dome.git", :tag => "v1.3.0",
+  depends_on "sdl2"
   
-  bottle do
-    root_url "https://github.com/avivbeeri/dome/releases/download/v1.3.0/"
-    cellar :any_skip_relocation
-    sha256 "7c53dc645face3b10d63101120bb0fcc2a0147529a5d1e2ea28fda1872b498a7" => :catalina
-  end
-
   def install
+    system "make"
     bin.install "dome"
   end
 
