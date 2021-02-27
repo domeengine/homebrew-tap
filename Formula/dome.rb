a69cafcbb7ef70ca0a526e4dd965dc66cb68ddb7
Aviv Beeri <avbeeri@gmail.com>
2021-02-27 16:07:15 +0000

    updating formula to 1.5.2

--- Formula/dome.rb
+++ Formula/dome.rb
@@ -4,8 +4,8 @@
 class Dome < Formula
   desc "The comfortable framework for making games in Wren."
   homepage "https://domeengine.com"
-  url "https://github.com/avivbeeri/dome.git", :using => :git, :tag => "v1.5.1"
-  version "1.5.1"
+  url "https://github.com/avivbeeri/dome.git", :using => :git, :tag => "v1.5.2"
+  version "1.5.2"
   sha256 ""
   license "MIT"
   depends_on "sdl2"
